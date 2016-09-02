1751e5e5f0613cdbe745ccdcee04264b5e37fb27
mbr84 <matthew.bramfeld@gmail.com>
2016-09-01 17:12:47 -0700

    devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   namespace :api, defaults: { format: :json } do
     resources :families, only: [:create, :show, :update]
     resources :users, only: [:create, :show, :update, :index]
