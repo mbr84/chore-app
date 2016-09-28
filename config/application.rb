de2081448c272a391f8f03c799f665a708c70671
mbr84 <matthew.bramfeld@gmail.com>
2016-09-27 20:05:06 -0700

    graphql directories and setup

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module ChoreApp
       resource '*', :headers => :any, :methods => [:get, :post, :options]
     end
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths << Rails.root.join('app', 'graph', 'types')  
   end
 end
